f8b7bb010c20d0319fc7b118077badc50204eb00
Thomas <aviosys16484847@gmail.com>
2021-05-13 13:09:00 +0800

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -2,58 +2,71 @@ const ipc = require('electron').ipcRenderer;
 const buttonCreated1 = document.getElementById('upload');
 const buttonCreated = document.getElementById('ipscan');
 const sendmsg = document.getElementById('posmsg');
-const ipcinfo = JSON.parse('{"group":{"007650000001":{"devip":"192.168.100.1","devport":"80","ver":"1.27"}}}');
-
+//const ipcinfo = JSON.parse('{"group":{"007650000001":{"devip":"192.168.100.1","devport":"80","ver":"1.27"}}}');
+const ipcinfo = [];
+var Total = '';
+var num = 0;
+var flg = 0;
 buttonCreated.addEventListener('click', function (event) {
    ipc.send('UDPGO');
 });
 
 ipc.on('UDPGO-reply', (event, arg) => {
-    // console.log(arg) // prints "pong"
-    var listT = document.getElementById("sel");
-    var option = document.createElement("option");    
-    var group = "group";
-    var info = arg.split(",");
-    var devinfo={};
-    smac=info[2].replace(/-/g,'');
-    var MAC=smac.toUpperCase();
-    //netmask=infos[4];
-    //gateway=infos[5];
-    ipport=info[3].split(":");
-    ip=ipport[0];
-    port=ipport[1];
-    version=info[8];
+   var listT = document.getElementById("sel");
+  var option = document.createElement("option");
 
-   
+  listT.options.length = 0;
+  //console.log(arg.length);
 
-    if(MAC.indexOf('9076') == -1)
-    {
-      devinfo = {[group]:{[MAC]:{"devip":ip,"devport":port,"ver":version}}};
-      
-      ipcinfo[group][MAC]=devinfo[group][MAC];
-       delete ipcinfo[group]['007650000001'];
-      //console.log(ipcinfo);
-      for(var MAC in devinfo[group]){
-        ip= devinfo[group][MAC].devip;
-        port= devinfo[group][MAC].devport;
-        devver = devinfo[group][MAC].ver;
-        //found=""; dift="NO";
-          for(var maci in ipcinfo[group])
-          {
-            if(MAC==maci)
-            {  
-              //found="found";
-              if(ipcinfo[group][maci].devip!=devinfo[group][maci].devip) {  ipcinfo[group][maci].devip=devinfo[group][maci].devip;   }
-              if(ipcinfo[group][maci].devport!=devinfo[group][maci].devport) { ipcinfo[group][maci].devport=devinfo[group][maci].devport;   }
-            }
-          }
-        
+  info = arg.split(",");
+  var devinfo;
+  var smac=info[2].replace(/-/g,'');
+  var MAC=smac.toUpperCase();
+  var ipport=info[3].split(":");
+  var ip=ipport[0];
+  var port=ipport[1];
+  var version=info[8];
+  devinfo = MAC+','+ip+','+version;
+  console.log(devinfo);
+  setTimeout(function(){
+
+  option.text = devinfo;
   
-        option.text = MAC+","+ip+","+port+","+devver;
-        listT.add(option);       
-        
+  if(listT.length > 1)
+  {
+    for(var i=0; i < listT.length; i++)
+    {
+      var infoA = listT.options[listT.length - 1].innerHTML;
+      var infoB = listT.options[i].innerHTML;
+      if(infoA == infoB)
+      {
+        if(flg == 0)
+        {
+          infoB = infoA;
+          flg = 1;
+        }
+        else if(flg == 1)
+        {
+          listT.remove(listT.length - 1);
+        }
       }
+      else
+      {
+        listT.add(option);
+        flg = 0;
+      }
+
     }
+  }
+  else
+  {
+    listT.add(option);
+  }
+
+
+
+
+}, 2000);
 
   
   })
